d5c9d4ec8c68449f0073ab1540977c095e171cf6
Unknown <blkfinch@gmail.com>
2018-07-29 14:24:46 -0400

    started work on blocking manager
    used to load various scenes at the different locations

---
+++ StoryTeller/Assets/Scripts/TestBlocking.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBlocking : MonoBehaviour {
+
+	public GameObject[] blockingPrefabs;
+	public int sceneToLoad;
+
+	private GameObject setting;
+
+	void Start(){
+		setting = Instantiate(blockingPrefabs[sceneToLoad]) as GameObject;
+	}
+
+
+}
